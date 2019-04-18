ed1389482e5e0a1d5e4c1e2d5572d534caa924e5
rob <robmathew21@gmail.com>
2019-04-18 16:51:41 -0500

    updated jumbotron

--- assets/app.js
+++ assets/app.js
@@ -42,7 +42,23 @@ $("#add-flight-btn").on("click", function(event) {
      console.log(frequency);
 
      // Still need to figure out modal
-     alert("Bullet Train Added");
+     var modal = document.getElementById('myModal');
+     var btn = document.getElementById("myBtn");
+     var span = document.getElementsByClassName("close")[0];
+
+     btn.onclick = function() {
+       modal.style.display = "block";
+     }
+
+     span.onclick = function() {
+       modal.style.display = "none";
+     }
+
+     window.onclick = function() {
+       if (event.target === modal) {
+         modal.style.display = "none";
+       }
+     }
 
      // Clears all of the text-boxes
      $("#train-name-input").val("");
